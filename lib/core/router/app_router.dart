33d3606830696b34055a795ca0a6e0a150140c84
liuchangchxy <1344967911@qq.com>
2026-04-16 21:48:11 +0800

    feat: add event creation and edit routes to router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:calendar_todo_app/ui/pages/home/home_page.dart';
 import 'package:calendar_todo_app/ui/pages/settings/settings_page.dart';
+import 'package:calendar_todo_app/ui/pages/event/event_create_page.dart';
+import 'package:calendar_todo_app/ui/pages/event/event_edit_page.dart';
+import 'package:calendar_todo_app/domain/models/calendar_event_adapter.dart';
 
 abstract final class AppRouter {
   static final GoRouter router = GoRouter(
@@ -16,6 +19,30 @@ abstract final class AppRouter {
         path: '/settings',
         name: 'settings',
         builder: (context, state) => const SettingsPage(),
+      ),
+      GoRoute(
+        path: '/event/new',
+        name: 'eventCreate',
+        builder: (context, state) {
+          final start = state.uri.queryParameters['start'];
+          final end = state.uri.queryParameters['end'];
+          return EventCreatePage(
+            initialStart: start != null
+                ? DateTime.fromMillisecondsSinceEpoch(int.parse(start))
+                : DateTime.now(),
+            initialEnd: end != null
+                ? DateTime.fromMillisecondsSinceEpoch(int.parse(end))
+                : DateTime.now().add(const Duration(hours: 1)),
+          );
+        },
+      ),
+      GoRoute(
+        path: '/event/edit',
+        name: 'eventEdit',
+        builder: (context, state) {
+          final event = state.extra as CalendaEventAdapter;
+          return EventEditPage(event: event);
+        },
       ),
     ],
   );
