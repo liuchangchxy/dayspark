0ae192037807daaa7b96cc2cea1254b509782c67
liuchangchxy <1344967911@qq.com>
2026-04-30 02:56:51 +0800

    fix: 移除因 DateTimeRange 改 String key 后的 unused import

--- lib/domain/providers/ai_scheduler_provider.dart
+++ lib/domain/providers/ai_scheduler_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dayspark/domain/providers/ai_provider.dart';
 import 'package:dayspark/domain/providers/events_provider.dart';
--- lib/domain/providers/events_provider.dart
+++ lib/domain/providers/events_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:drift/drift.dart' hide Column;
 import 'package:dayspark/data/local/database/app_database.dart';
